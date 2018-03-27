0c991f1fa17f9c48f16677488a7be322746858e3
yuzhangiot <yuzhangiot@gmail.com>
2018-03-27 14:47:59 +0800

    add WarpFieldOptimizer class interface.

---
+++ Core/src/WarpFieldOptimizer.cpp
@@ -0,0 +1,2 @@
+#include "WarpFieldOptimizer.h"
+
---
+++ Core/src/WarpFieldOptimizer.h
@@ -0,0 +1,50 @@
+#ifndef WARP_FIELD_OPTIMIZER_H_
+#define WARP_FIELD_OPTIMIZER_H_
+
+#include "WarpField.h"
+
+
+class WarpFieldOptimizer
+{
+public:
+	WarpFieldOptimizer();
+	~WarpFieldOptimizer();
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
