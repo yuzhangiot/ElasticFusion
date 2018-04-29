cc1b1e10e68ae3189de461957bce0a43c8528e74
yuzhangiot <yuzhangiot@gmail.com>
2018-04-29 15:39:53 +0800

    update run script.

--- GUI/run_ply.sh
+++ GUI/run_ply.sh
@@ -1,1 +1,1 @@
-./ElasticFusion -l /mnt/hd0/3data/raw/upperbody/upperbody.klg -o -f -d 1.2 -t 2000 -ply /mnt/hd0/3data/ply/0
+./ElasticFusion -l /mnt/hd0/3data/raw/upperbody/upperbody.klg -o  -d 1.2 -t 2000 -ply /home/joseph/workbench/JointFusion/texfu/output/canonical_frames
